2e1397a021fd4e837a305a2ac8d203d8c0245c92
Ezgi Adıgüzel <ezgiadiguzel01@gmail.com>
2023-10-31 20:18:00 +0300

    Add project files.

---
+++ Daire/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class CircleDrawer
+{
+    public void DrawCircle(int radius)
+    {
+        double angleIncrement = 0.01;
+        for (double angle = 0; angle < 2 * Math.PI; angle += angleIncrement)
+        {
+            int x = (int)(radius * Math.Cos(angle));
+            int y = (int)(radius * Math.Sin(angle));
+
+            Console.SetCursorPosition(radius + x, radius - y);
+            Console.Write("*");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Dairenin yarıçapını girin: ");
+        int radius = int.Parse(Console.ReadLine());
+
+        CircleDrawer circleDrawer = new CircleDrawer();
+        circleDrawer.DrawCircle(radius);
+
+        Console.ReadLine(); // Çizim ekranda kalsın diye bekleyin
+    }
+}
